aaa33298d7a5e03f87f3e720cf0abe41df9e0f76
toshiki-nakajima <72725615+toshiki-nakajima@users.noreply.github.com>
2025-03-21 19:59:49 +0900

    style

--- src/components/LiffLogin.tsx
+++ src/components/LiffLogin.tsx
@@ -9,12 +9,17 @@ const LiffLogin: React.FC<LiffLoginProps> = ({ onLogin, isLoaded }) => {
   return (
     <div>
       <p>LINEにログインしてUIDを取得し、QRコードを生成します。</p>
-      <button 
-        onClick={onLogin} 
-        disabled={!isLoaded}
-      >
-        LINEでログイン
-      </button>
+        <div className="login-container">
+            <button
+                onClick={onLogin}
+                disabled={!isLoaded}
+                className="login-button"
+            >
+                <img src="/line_44.png" alt="LINEでログイン"/>
+                <div className="vertical-line"></div>
+                <span className="login-message">LINEでログイン</span>
+            </button>
+        </div>
     </div>
   );
 };
